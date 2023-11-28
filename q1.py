527039299292716706d09049d7be4666f82104f3
adithgopal24 <azg5941@psu.edu>
2023-11-28 17:59:47 -0500

    Creating q-table, but receiving negative values

--- q1.py
+++ q1.py
@@ -4,12 +4,11 @@ import numpy as np
 def run_BJack_Q():
     # Start Q-learning code
     env = gym.make("Blackjack-v1", natural=False, sab=False)  # Initializing environments
-    observation1, info = env.reset()
+    observation, info = env.reset()
     terminated = False  # Will be true if we win Blackjack
     truncated = False  # Will be true when the "Actions" threshold is met
-
-    #q_table = np.zeros((env.observation_space[0].n,  env.action_space[0].n))
-    q_table = np.zeros((32, 32, 2))
+    #q_table = np.zeros([env.observation_space[0], env.action_space.n])
+    q_table = np.zeros((32, 11, 2))
     alpha = 0.1
 
 
@@ -17,13 +16,12 @@ def run_BJack_Q():
     while not terminated and not truncated:
         for _ in range(1000):
             #implement q-learning
-            action = env.action_space.sample() # Blackjack actions: Take a card, or no.
-            #action = np.argmax(q_table[state])
-            state, reward, terminated, truncated, info = env.step(action)
+            action = env.action_space.sample() # Blackjack actions: Take a card, or no. 0 or 1
+            observation, reward, terminated, truncated, info = env.step(action)
             #q_table[state, action] += (reward + np.max([q_table[state]]) - q_table[state, action])
-            q_table[state, action] += (1-alpha)*(q_table[state,action]) + alpha*reward
+            q_table[observation, action] += (1-alpha)*(q_table[observation,action]) + alpha*reward
             if terminated or truncated:
-                observation2, info = env.reset()
+                observation, info = env.reset()
     env.close()
     print(q_table)
     # End Q-learning code
