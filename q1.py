7c81981ef5a971717eb5051ee8b9b17264676f4c
adithgopal24 <azg5941@psu.edu>
2023-11-29 11:21:21 -0500

    Adding comments to code

--- q1.py
+++ q1.py
@@ -5,34 +5,22 @@ np.set_printoptions(suppress=True)
 def run_BJack_Q():
     # Start Q-learning code
     env = gym.make("Blackjack-v1", natural=False, sab=False)  # Initializing environments
-    
-    #env = gym.make("Blackjack-v1", natural=False, sab=False, render_mode="human")  # Initializing environments
     stateNEW, info = env.reset()
     terminated = False  # Will be true if we win Blackjack
     truncated = False  # Will be true when the "Actions" threshold is met
-    #q_table = np.zeros([env.observation_space[0], env.action_space.n])
     q_table = np.zeros([32, 11, 2,2])
-    alpha = 0.8
+    alpha = 0.8 #will be used when adding values to the q-table
     eta = .4
     epilson = 10
     stateOLD = stateNEW
     for _ in range(1000000):
         #implement q-learning
-        action = env.action_space.sample()
-        '''
-        if np.random.random() < epilson:
-             # Blackjack actions: Take a card, or no. 0 or 1
-        else:
-            action = int(np.argmax(q_table[observation]))'''
+        action = env.action_space.sample() #get a sample Blackjack action
         stateNEW, reward, terminated, truncated, info = env.step(action)
-        #print(stateOLD)
-        #print(q_table[stateOLD[0],stateOLD[1],stateOLD[2], action])
-        #q_table[state, action] += (reward + np.max([q_table[state]]) - q_table[state, action])
-        q_table[stateOLD[0],stateOLD[1],stateOLD[2], action]    = (1-alpha)*(q_table[stateOLD[0],stateOLD[1],stateOLD[2], action] ) + alpha*(reward+eta*np.max(q_table[stateNEW[0],stateNEW[1],stateNEW[2]] ))
-        #q_table[stateOLD[0],stateOLD[1],stateOLD[2], action] +=1
+        q_table[stateOLD[0],stateOLD[1],stateOLD[2], action] = (1-alpha)*(q_table[stateOLD[0],stateOLD[1],stateOLD[2], action] ) + alpha*(reward+eta*np.max(q_table[stateNEW[0],stateNEW[1],stateNEW[2]] )) #adding new estimate to the q-table, creating a running average
         if terminated or truncated:
             observation, info = env.reset()
-        epilson=max(0,epilson-0.00001)
+        epilson=max(0,epilson-0.00001) #making sure actions are taken randomly
         stateOLD = stateNEW
     env.close()
     print(q_table)
